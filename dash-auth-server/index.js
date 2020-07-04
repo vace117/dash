667b85b00240ff3d7288b87e00de143d56619355
Val Blant <val.blant@canada.ca>
2020-07-03 20:35:54 -0500

    Pusher end-to-end integration test works!

--- dash-auth-server/index.js
+++ dash-auth-server/index.js
@@ -6,13 +6,15 @@ require('log-timestamp')(() => {
     const messageTime = new Date()
     return `[${messageTime.toLocaleString('en-CA')} <${messageTime.getMilliseconds()}ms>]`
 })
-  
+
+const fs = require('fs')
+const SECRETS = JSON.parse( fs.readFileSync('secrets-1027455.json') )
 
 const PusherServer = new require('pusher')
 const pusher = new PusherServer({
     appId: '1027455',
     key: 'db1489f973f891a9072e',
-    secret: 'b340887912f16da9a0ca',
+    secret: SECRETS['pusher-dash-secret'],
     cluster: 'us2',
     useTLS: true
 })
@@ -30,7 +32,7 @@ server.post('/pusher/auth', function(req, res) {
     const channel = req.body.channel_name
     const password = req.body.password
 
-    if ( password === 'GOODY') {
+    if ( password === SECRETS['dash-auth-server-password'] ) {
         const auth = pusher.authenticate(socketId, channel)
         res.send(auth)
     }
--- dash-player/tests/server/pusher-pubsub-client.js
+++ dash-player/tests/server/pusher-pubsub-client.js
@@ -1,38 +1,75 @@
+/** ***********************************************************************
+ * dash-auth-server needs to be running to use this test!
+ *
+ * This verifies that our clients can publish messages to the private channel
+ *
+ * *************************************************************************/
+
 // Set logging format
 //
-
-/* eslint-disable no-unused-vars */
-
-const PusherClient = require('pusher-js')
-
 require('log-timestamp')(() => {
   const messageTime = new Date()
   return `[${messageTime.toLocaleString('en-CA')} <${messageTime.getMilliseconds()}ms>]`
 })
+const chalk = require('chalk')
+const PusherClient = require('pusher-js')
+// PusherClient.logToConsole = true
+const sendClient = _createClient()
+const receiveClient = _createClient()
 
-const CHANNEL_NAME = 'dash-player-control'
+const CHANNEL_NAME = 'private-player-control'
+const SUCCESSFUL_TEST = 'SUCCESSFUL_TEST'
 
 async function testPubSub () {
-  console.log('Initializing pusher client...')
-  PusherClient.logToConsole = true
-  const pusher = new PusherClient('db1489f973f891a9072e', {
-    cluster: 'us2',
-    authEndpoint: 'http://localhost:5000/pusher/auth',
-    auth: {
-      params: { password: 'GOODY' }
-    }
-  })
-
-  console.log('Subscribing...')
-  const channel = pusher.subscribe('private-player-control')
-
-  channel.bind('pusher:subscription_succeeded', function () {
-    console.log('Subscribtio suceeded.')
-  })
-
-  channel.bind('PLAY', function (data) {
-    console.log(JSON.stringify(data))
-  })
+  _setupReceiveClient()
 }
 
 testPubSub()
+
+function _setupReceiveClient () {
+  console.log('Subscribing the receiving client...')
+
+  receiveClient
+    .subscribe(CHANNEL_NAME)
+    .bind('pusher:subscription_succeeded', function () {
+      _sendTestCommand()
+
+      console.log('Receiver waiting for test message...')
+    })
+    .bind('client-test-command', function (data) {
+      console.log(`Received test data: ${JSON.stringify(data)}`)
+      if (data.payload === SUCCESSFUL_TEST) {
+        console.log(chalk.greenBright('TEST SUCCESS!!!'))
+        sendClient.disconnect()
+        receiveClient.disconnect()
+      } else {
+        console.log(chalk.redBright('You suck!! Test failure.'))
+      }
+    })
+}
+
+function _sendTestCommand () {
+  console.log('Subscribing the sending client...')
+  const channel = sendClient.subscribe(CHANNEL_NAME)
+
+  channel.bind('pusher:subscription_succeeded', function () {
+    console.log('Missiles away...')
+    channel.trigger('client-test-command', { payload: SUCCESSFUL_TEST })
+  })
+}
+
+function _createClient () {
+  return new PusherClient('db1489f973f891a9072e', {
+    cluster: 'us2',
+    authEndpoint: 'http://localhost:5000/pusher/auth',
+    auth: {
+      params: { password: _readPassword() }
+    }
+  })
+}
+
+function _readPassword () {
+  if (process.env.DASH_AUTH_PASSWORD) {
+    return process.env.DASH_AUTH_PASSWORD
+  } else throw new Error('DASH_AUTH_PASSWORD environment variable is not set!')
+}
