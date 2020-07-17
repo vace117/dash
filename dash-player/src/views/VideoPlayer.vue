51be78c5be989bbb45ea8166318c647dabc06128
Val Blant <val.blant@canada.ca>
2020-07-17 04:02:58 -0500

    Video playback working

--- dash-player/src/views/VideoPlayer.vue
+++ dash-player/src/views/VideoPlayer.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="videoPlayerPanel">
-    Video Player is here
+    <video id="videoPlayer" controls></video>
+    <p/>
+    <b-button @click="goBack" size="lg" variant="warning">GO BACK</b-button>
   </div>
 </template>
 
@@ -14,7 +16,33 @@ export default {
     }
   },
 
+  mounted () {
+    const dashjs = require('dashjs')
+    this.dashPlayer = dashjs.MediaPlayer().create()
+    this.dashPlayer.initialize(
+      document.querySelector('#videoPlayer'),
+      this.$store.state.selectedVideoUrl,
+      false
+    )
+    this.dashPlayer.on('playbackPlaying', this.videoPlayingEventHandler)
+    this.dashPlayer.on('playbackPaused',  this.videoPausedEventHandler)
+
+    // TODO:
+    // this.dashPlayer.seek(200)
+  },
+
   methods: {
+    goBack () {
+      this.$router.go(-1)
+    },
+
+    videoPlayingEventHandler (payload) {
+      console.log('Event recieved: ' + JSON.stringify(payload))
+    },
+
+    videoPausedEventHandler (payload) {
+      console.log('Event recieved: ' + JSON.stringify(payload))
+    }
   },
 
   computed: {
@@ -25,4 +53,9 @@ export default {
 </script>
 
 <style scoped>
+  video {
+      width: 640px;
+      height: 360px;
+      background-color: #666666;
+  }
 </style>
