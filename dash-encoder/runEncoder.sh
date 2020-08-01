d27c43f4812a9aec99352407c1383f1bbc89da1e
Val Blant <val.blant@canada.ca>
2020-08-01 04:28:55 -0500

    runEncoder.sh script takes video directory param

--- dash-encoder/runEncoder.sh
+++ dash-encoder/runEncoder.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-docker stop dash-encoder 2>&1 > /dev/null
-docker rm   dash-encoder 2>&1 > /dev/null
+if [ -z "$1" ] ; then
+    printf "\n   Please specify the directory where your input videos are!\n\n"
+    exit 1
+fi
 
+VIDEO_DIR=`readlink -f $1`
+
+if [ ! -d "$VIDEO_DIR" ] ; then 
+    printf "\n   '$VIDEO_DIR' does not exist!\n\n"
+    exit 2
+fi
+
+docker stop dash-encoder &> /dev/null
+docker rm   dash-encoder &> /dev/null
+
+
+# Use the volume mount below for development:
+#     -v /home/val/dash/dash-encoder/scripts:/home/dash/custom_scripts \
 docker run -it \
-    -v /home/val/dash/dash-encoder/VIDEO:/mnt/video \
-    -v /home/val/dash/dash-encoder/scripts:/home/dash/custom_scripts \
+    -v $VIDEO_DIR:/mnt/video \
     --name dash-encoder \
     vace117/dash-encoder
