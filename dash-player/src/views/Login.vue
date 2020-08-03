26a0ccb657688a3784847974a36baad731ceeb98
Val Blant <val.blant@canada.ca>
2020-08-03 04:55:36 -0500

    Adding a list of live users on a channel on VideoPlayer page

--- dash-player/src/views/Login.vue
+++ dash-player/src/views/Login.vue
@@ -2,8 +2,20 @@
   <b-row align-h="center" class="text-center">
     <b-col id="loginPanel" class="bigScreenText">
       >>> SPEAK FRIEND AND ENTER &lt;&lt;&lt;
+      <hr class="fatHR"/>
       <br/>
-      <b-form-input v-model="password" @keyup.enter="login" lazy type="password" class="bigInputText" autofocus />
+      <b-row align-v="center">
+        <b-col cols="3" class="text-left">Thy Name: </b-col>
+        <b-col cols="7" class="p-3">
+          <b-form-input v-model="userName" @keyup.enter="login" lazy type="text" class="bigInputText" autofocus />
+        </b-col>
+      </b-row>
+      <b-row align-v="center">
+        <b-col cols="3" class="text-left">Thy Secret:</b-col>
+        <b-col cols="7" class="p-3">
+          <b-form-input v-model="password" @keyup.enter="login" lazy type="password" class="bigInputText" />
+        </b-col>
+      </b-row>
       <p/>
       <b-button @click.prevent="login" type="submit" size="funsize" variant="primary">LOGIN</b-button>
     </b-col>
@@ -16,6 +28,18 @@ import { mapFields } from 'vuex-map-fields'
 export default {
   methods: {
     login () {
+      const userName = this.$store.state.userName
+      if (!userName || userName.length === 0) {
+        this.$store.commit('updateErrors', 'Please enter a valid username!')
+        return
+      }
+
+      const password = this.$store.state.password
+      if (!password || password.length === 0) {
+        this.$store.commit('updateErrors', 'Please enter the password!')
+        return
+      }
+
       this.$store.dispatch('login')
     }
   },
@@ -26,7 +50,7 @@ export default {
   },
 
   computed: {
-    ...mapFields(['password'])
+    ...mapFields(['userName', 'password'])
   }
 
 }
--- dash-player/src/views/VideoPlayer.vue
+++ dash-player/src/views/VideoPlayer.vue
@@ -10,15 +10,39 @@
       </div>
       <div v-show="pubSubInitCompletedInd">
 
-        <!-- Video Player -->
-        <video id="videoPlayer" controls></video>
-        <p/>
-        <b-button @click="goBack" size="lg" variant="secondary">GO BACK</b-button>
-        <p/>
-
-        <!-- Current Link -->
-        <hr class="mediumHR"/>
-        <div class="smallestScreenText text-center">{{selectedVideoUrl}}</div>
+        <b-row no-gutters>
+          <b-col cols="10" class="p-3">
+            <!-- Video Player -->
+            <video id="videoPlayer" controls></video>
+          </b-col>
+          <b-col class="screenSectionBorder">
+            <b-row>
+              <b-col>
+                <span class="smallerScreenText nowrap">Team Roster</span>
+                <hr class="mediumHR"/>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col class="text-left pl-1 otherColor smallestScreenText">
+                <ul class="noBullets">
+                  <li style="font-size: 1.3em">{{userName}}</li>
+                  <li v-for="remoteUser in crewRoster" :key="remoteUser">
+                    {{remoteUser}}
+                  </li>
+                </ul>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+        <b-row no-gutters class="mt-3">
+          <b-col>
+            <!-- Current Link -->
+            <hr class="mediumHR"/>
+            <div class="smallestScreenText text-center">{{selectedVideoUrl}}</div>
+            <p/>
+            <b-button @click="goBack" size="lg" variant="secondary">GO BACK</b-button>
+          </b-col>
+        </b-row>
       </div>
     </b-col>
   </b-row>
@@ -31,10 +55,18 @@ import createOrSubscribeToChannelForVideo from '@/pubsub/pusher.js'
 
 const _ = require('lodash/core')
 
+const KEEP_ALIVE_PERIOD_SECONDS = 15
+const NodeCache = require('node-cache')
+const liveUserCache = new NodeCache({
+  stdTTL:      KEEP_ALIVE_PERIOD_SECONDS + 5,
+  checkperiod: KEEP_ALIVE_PERIOD_SECONDS / 3
+})
+
 export default {
   data () {
     return {
-      pubSubInitCompletedInd: false
+      pubSubInitCompletedInd: false,
+      crewRoster: []
     }
   },
 
@@ -50,9 +82,24 @@ export default {
       password:         this.$store.state.password
     })
       .then(channel => {
+        // Register Pub/Sub message processors
+        //
         this.pubSubChannel = channel
-        this.pubSubChannel.bind('client-video-command', this.processReceivedCommand)
+        this.pubSubChannel.bind('client-video-command',  this.processReceivedCommand)
+        this.pubSubChannel.bind('client-user-keepalive', this.processUserKeepAlive)
         this.pubSubInitCompletedInd = true
+
+        // Register Live User cache callbacks and begin sending out KeepAlive messages
+        //
+        liveUserCache.on('set',     this.updateCrewRoster)
+        liveUserCache.on('expired', this.updateCrewRoster)
+        liveUserCache.on('flush',   this.updateCrewRoster)
+        liveUserCache.flushAll()
+        this.sendAliveMessage()
+        this.keepAliveTimer = setInterval(
+          () => this.sendAliveMessage(),
+          KEEP_ALIVE_PERIOD_SECONDS * 1000
+        )
       })
       .catch(error => this.$store.commit('updateErrors', error))
   },
@@ -68,6 +115,8 @@ export default {
     },
 
     _tearDown () {
+      clearInterval(this.keepAliveTimer)
+
       this.pubSubChannel.pusher.disconnect()
       this.pubSubChannel = null
 
@@ -86,6 +135,22 @@ export default {
       )
       this.dashPlayer.on('playbackPlaying', this.broadcastPlayerEvent)
       this.dashPlayer.on('playbackPaused',  this.broadcastPlayerEvent)
+    },
+
+    updateCrewRoster () {
+      this.crewRoster = liveUserCache.keys()
+    },
+
+    sendAliveMessage () {
+      console.log('Broadcasting our KeepAlive...')
+      this.pubSubChannel.trigger('client-user-keepalive', this.$store.state.userName)
+    },
+
+    processUserKeepAlive (remoteUserName) {
+      if (remoteUserName !== this.$store.state.userName) {
+        console.log(`Received KeepAlive for ${remoteUserName}`)
+        liveUserCache.set(remoteUserName, null)
+      }
     },
 
     processReceivedCommand (command) {
@@ -142,7 +207,7 @@ export default {
   },
 
   computed: {
-    ...mapFields(['selectedVideoUrl']),
+    ...mapFields(['userName', 'selectedVideoUrl']),
 
     errorsPresent () {
       return this.$store.getters.errorsPresent
@@ -154,7 +219,11 @@ export default {
 
 <style scoped>
   video {
-      width: 65%;
+      width: 80%;
       background-color: #666666;
+  }
+
+  .noBullets {
+    list-style-type: none;
   }
 </style>
