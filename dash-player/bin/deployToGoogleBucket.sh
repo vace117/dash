75807dcdff6c0ec4143ed37b3d711600cac0a520
val <vace117@yahoo.ca>
2020-08-12 05:06:48 -0500

    Adding build.txt to deployments

--- dash-player/bin/deployToGoogleBucket.sh
+++ dash-player/bin/deployToGoogleBucket.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 printf "Preparing production deploy..."
-rm -rf dist dash
+rm -rf dist
 npm run build
+
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_REV=`git rev-parse HEAD`
+echo "${GIT_BRANCH}:${GIT_REV}" > dist/build.txt
 
 printf "\n\nDeploying to 'gs://fungate'...\n"
 gsutil -m rm -r 'gs://fungate/*'
-gsutil -m -h "Cache-Control:no-cache,max-age=0" cp -r dist/*  gs://fungate
+gsutil -m -h "Cache-Control:no-cache,max-age=0" cp -r dist/*  'gs://fungate'
