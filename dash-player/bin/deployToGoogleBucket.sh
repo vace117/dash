f39ff8611cb99e541c8eaceeb3e982beede0ae74
val <vace117@yahoo.ca>
2020-08-12 04:57:07 -0500

    Improving Google Bucket deploy

--- dash-player/bin/deployToGoogleBucket.sh
+++ dash-player/bin/deployToGoogleBucket.sh
@@ -5,5 +5,5 @@ rm -rf dist dash
 npm run build
 
 printf "\n\nDeploying to 'gs://fungate'...\n"
-gsutil rm -r 'gs://fungate/*'
+gsutil -m rm -r 'gs://fungate/*'
 gsutil -m -h "Cache-Control:no-cache,max-age=0" cp -r dist/*  gs://fungate
